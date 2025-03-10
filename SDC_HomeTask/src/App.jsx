070f7525425a589212108ee9a744fb1802c66938
artem <artsiomharminovich05@gmail.com>
2025-03-10 17:15:14 +0200

    Done Home Task 2

--- SDC_HomeTask/src/App.jsx
+++ SDC_HomeTask/src/App.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import {Component} from 'react';
 import './App.css';
+import Header from "./Component/Header.jsx";
+import Footer from "./Component/Footer.jsx";
+import OrderMainMenu from "./Component/OrdersJSX/OrderContent.jsx";
 
-function App() {
-    return (
-        <div>
-            <form>
-                <label htmlFor="street">Street</label>
-                <input type="text" id="street" name="street" />
-                <label htmlFor="house">House</label>
-                <input type="text" id="house" name="house" />
-                <div className="button-wrapper">
-                    <button type="submit">
-                        Order
-                        <span className="tooltip">Нажмите, чтобы оформить заказ!</span>
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <Header/>
+                <OrderMainMenu/>
+                <Footer/>
+            </>
+        )
+    }
 }
 
-export default App;
