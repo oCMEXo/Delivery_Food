a3fb2e126d689da82e03fb5f42fede81d6e29479
artem <artsiomharminovich05@gmail.com>
2025-04-28 22:29:08 +0300

    function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Menu from "./Page/Menu.jsx";
 
-export default class App extends Component {
-    render() {
-        return (
-            <Menu />
-        );
-    }
+export default function App() {
+    return (
+        <Menu/>
+    );
 }
