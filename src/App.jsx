9396f7c7594c06f2c79b716b260723cff6b0ba07
artem <artsiomharminovich05@gmail.com>
2025-05-02 14:58:30 +0300

    Home page with css comment

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import Menu from "./Page/Menu.jsx";
 import Home from "./Page/Home.jsx";
+import Footer from "./Components/Layout/Footer.jsx";
 
 export default function App() {
     return (
-        <Menu/>
+        <Home />
     );
 }
