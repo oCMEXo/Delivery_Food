115d89c705f4400d20af74425426b4d7ab85a842
artem <artsiomharminovich05@gmail.com>
2025-05-14 16:50:28 +0300

    Styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 import Menu from "./Page/Menu.jsx";
 import Home from "./Page/Home.jsx";
 import Footer from "./Components/Layout/Footer.jsx";
+import Login from "./Page/Login.jsx";
+import CreateUser from "./Page/CreateUser.jsx";
 
 export default function App() {
     return (
-        <Home />
+        <CreateUser />
     );
 }
