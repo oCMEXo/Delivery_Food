90df055dd19d31d37e935cd9b6c8269e9b9bbe43
artem <artsiomharminovich05@gmail.com>
2025-05-14 15:39:25 +0300

    CreateUser.jsx

---
+++ src/Page/CreateUser.jsx
@@ -0,0 +1,30 @@
+import Header from "../Components/Layout/Header.jsx";
+import Footer from "../Components/Layout/Footer.jsx";
+
+
+export default function CreateUser(){
+    return (
+        <>
+            <Header />
+            <section className="login-font">
+                <h1>Create User</h1>
+                <form action="">
+                    <div className="input_Info">
+                        <div className="email">
+                            <label htmlFor="email">User Name</label>
+                            <input type="email" />
+                        </div>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" />
+                        </div>
+                    </div>
+                    <div className="btn_Form">
+                        <button type="submit" className='submit'>Create</button>
+                    </div>
+                </form>
+            </section>
+            <Footer />
+        </>
+    )
+}
