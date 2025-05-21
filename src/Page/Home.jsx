f9c8dd72b232107242b458aca962080626bee1c2
artem <artsiomharminovich05@gmail.com>
2025-05-21 14:56:20 +0300

    Done hm6

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,13 +3,10 @@ import Header from "../Components/Layout/Header.jsx";
 import HomeComponent from "../Components/ContentMenu/HomeComponent.jsx";
 import Footer from "../Components/Layout/Footer.jsx";
 import { useAuth } from "../Components/hooks/use-auth.js";
-import { useDispatch } from "react-redux";
-import { removeUser } from "../Components/redux/slices/usersSlice.js";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-    const { isAuth, email } = useAuth();
-    const dispatch = useDispatch();
+    const { isAuth,  } = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
