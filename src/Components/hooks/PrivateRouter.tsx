92e5ee7c5919efc0d854be8601b4ef4119a58bc6
artem <artsiomharminovich05@gmail.com>
2025-06-06 21:18:36 +0300

    feat: Home task 8

--- src/Components/hooks/PrivateRouter.tsx
+++ src/Components/hooks/PrivateRouter.tsx
@@ -2,8 +2,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from './use-auth';
 
 const PrivateRoute = () => {
-    const isAuth = useAuth();
-
+    const {isAuth} = useAuth();
+    console.log(isAuth);
     return isAuth ? <Outlet /> : <Navigate to="/login" />;
 };
 
